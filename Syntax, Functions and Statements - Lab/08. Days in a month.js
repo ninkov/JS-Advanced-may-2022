a63063a8d660aa131b00c02209d96dc5ee4779c9
Ninkov <ninkov82@gmail.com>
2022-05-17 10:05:59 +0300

    Days in a month
    Write a JavaScript function to get the number of days in a month.
    The input comes as two numeric parameters. The first element is the month, the second is the year.
    The output must return the number of days in a month for a given year.
    
    Examples
    input: 1, 2012     output: 31

---
+++ Syntax, Functions and Statements - Lab/08. Days in a month.js
@@ -0,0 +1,13 @@
+function solve(month, year) {
+    let days = new Date(year, month, 0).getDate();
+    console.log(days);
+}
+// Month in JavaScript is 0-indexed (January is 0, February is 1, etc), 
+// but by using 0 as the day it will give us the last day of the prior
+// month. So passing in 1 as the month number will return the last day
+// of January, not February
+// Here January is 1 based
+//Day 0 is the last day in the previous month
+// Here January is 0 based
+// return new Date(year, month+1, 0).getDate();
+solve(1, 2012);
