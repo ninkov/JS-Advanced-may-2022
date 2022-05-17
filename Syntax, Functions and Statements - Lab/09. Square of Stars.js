f0815cf3bc6088300c752751b61d6c43b1b59528
Ninkov <ninkov82@gmail.com>
2022-05-17 10:14:37 +0300

    Square of Stars
    Write a function that prints a rectangle made of stars with variable size, depending on an input parameter. If there is no parameter specified, the rectangle should always be of size 5. Look at the examples to get an idea.
    The input comes as a single number argument.
    The output is a series of lines printed on the console, forming a rectangle of variable size.
     
    Examples
    input: 1        output     * *
                                         * *

---
+++ Syntax, Functions and Statements - Lab/09. Square of Stars.js
@@ -0,0 +1,11 @@
+function solve(input) {
+    let res = '';
+    for (let i = 0; i < input; i++) {
+        for (let j = 0; j < input; j++) {
+            res += '*' + ' ';
+        }
+        res+="\n";
+    }
+    console.log(res);
+}
+solve(2);
