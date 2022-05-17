371213c3ac1a3a27f8e5194efc11b80e978af8be
Ninkov <ninkov82@gmail.com>
2022-05-17 09:58:57 +0300

    Sum of Numbers N…M
    Write a JS function that takes two numbers n and m as an input and prints the sum of all numbers from n to m.
    The input comes as two string elements that need to be parsed as numbers.
    The output should return the sum.
    
    Examples
    
    input: '1', '5'      output: 15
    input: '-8', '20'      output: 174

---
+++ Syntax, Functions and Statements - Lab/06. Sum of Numbers N…M.js
@@ -0,0 +1,10 @@
+function solve(n, m) {
+n = Number(n);
+m=Number(m)
+    let result = 0;
+    for (let i = n; i <= m; i++) {
+        result += i;
+    }
+    console.log(result);
+}
+solve('1', '5');
