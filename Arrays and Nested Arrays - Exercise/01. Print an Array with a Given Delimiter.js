6ab39c7b6b4b0dc44cc7af6195f5a34e9ab477c4
Ninkov <ninkov82@gmail.com>
2022-05-22 18:22:47 +0300

    Print an Array with a Given Delimiter
    The input comes as two parameters – an array of strings and a string. The second parameter is the delimiter.
    The output is the elements of the array, printed on the console, each element separated from the others by the given delimiter.

---
+++ Arrays and Nested Arrays - Exercise/01. Print an Array with a Given Delimiter.js
@@ -0,0 +1,12 @@
+function solve(arr,str) {
+let result = arr.join(str)
+console.log(result);
+}
+solve(
+    ['One',
+        'Two',
+        'Three',
+        'Four',
+        'Five'],
+    '-'
+);
