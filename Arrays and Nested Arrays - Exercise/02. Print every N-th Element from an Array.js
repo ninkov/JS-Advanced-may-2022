56b25f77c31117687a11b0c322ce1c0a3515d183
Ninkov <ninkov82@gmail.com>
2022-05-23 06:12:09 +0300

    Print every N-th Element from an Array
    The input comes as two parameters – an array of strings and a number. The second parameter is N – the step.
    The output is every element on the N-th step starting from the first one. If the step is 3, you need to return the 1-st, the 4-th, the 7-th … and so on, until you reach the end of the array. 
    The output is the return value of your function and must be an array.

---
+++ Arrays and Nested Arrays - Exercise/02. Print every N-th Element from an Array.js
@@ -0,0 +1,10 @@
+function solve(arr, n) {
+  let result = [];
+  for (let i = 0; i < arr.length; i += n) {
+    result.push(arr[i]);
+  }
+  return result;
+}
+solve(["dsa", "asd", "test", "tset"], 2);
+solve(["1", "2", "3", "4", "5"], 6);
+solve(["5", "20", "31", "4", "20"], 2);
