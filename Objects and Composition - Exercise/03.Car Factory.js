63c23f29f2f30e1feb0bcc65dc75297fb8a2278b
Ninkov <ninkov82@gmail.com>
2022-05-29 23:09:40 +0300

    Car Factory
    Write a program that assembles a car by giving requirements out of existing components. The client will place an order in the form of an object describing the car. You need to determine which parts to use to fulfill the client’s order. You have the following parts in storage:
    An engine has power (given in horsepower) and volume (given in cubic centimeters). Both of these values are numbers. When selecting an engine, pick the smallest possible that still meets the requirements.
    Small engine: { power: 90, volume: 1800 }
    Normal engine: { power: 120, volume: 2400 }
    Monster engine: { power: 200, volume: 3500 }
    A carriage has a type and color. Both of these values are strings. You have two types of carriages in storage and can paint them any color.
    Hatchback: { type: 'hatchback', color: <as required> }
    Coupe: { type: 'coupe', color: <as required> }
    The wheels will be represented by an array of 4 numbers, each number represents the diameter of the wheel in inches. The size can only be an odd number. Round down any requirements you receive to the nearest odd number. 
    Input
    You will receive an object as an argument to your function. The format will be as follows:
    { model: <model name>,
      power: <minimum power>,
      color: <color>,
      carriage: <carriage type>,
      wheelsize: <size> }
    Output
    Return the resulting car object as a result of your function. See the examples for details.

---
+++ Objects and Composition - Exercise/03.Car Factory.js
@@ -0,0 +1,31 @@
+function carFactory(wantedCar) {
+  let engines = [
+    { power: 90, volume: 1800 },
+    { power: 120, volume: 2400 },
+    { power: 200, volume: 3500 },
+  ];
+  let carriages = [
+    { type: "hatchback", color: wantedCar.color },
+    { type: "coupe", color: wantedCar.color },
+  ];
+  
+  let wheelsize =
+    wantedCar.wheelsize % 2 == 1
+      ? wantedCar.wheelsize
+      : wantedCar.wheelsize - 1;
+console.log(wheelsize);
+  return {
+    model: wantedCar.model,
+    engine: engines.filter((e) => e.power >= wantedCar.power)[0],
+    carriage: carriages.filter((c) => c.type == wantedCar.carriage)[0],
+    wheels: Array(4).fill(wheelsize)//wheels: [wheelsize,wheelsize,wheelsize,wheelsize]
+  }
+}
+console.log(
+carFactory({
+  model: "VW Golf II",
+  power: 90,
+  color: "blue",
+  carriage: "hatchback",
+  wheelsize: 14,
+}));
