1efad13195b31ed8baba98d4d3cb0abdd5b33ab4
Ninkov <ninkov82@gmail.com>
2022-06-01 22:25:40 +0300

    Extract Parenthesis
    Write a JS function that when executed, extracts all parenthesized text from a target paragraph by given element ID. The result is a string, joined by "; " (semicolon, space).
    Input
    Your function will receive a string parameter, representing the target element ID, from which text must be extracted. The text should be extracted from the DOM.
    Output
    Return a string with all matched text, separated by "; " (semicolon, space).
    Examples

---
+++ DOM Introduction - Lab/07. Extract Parenthesis.js
@@ -0,0 +1,13 @@
+function extract(content) {
+  const text = document.getElementById("content").textContent;
+let pattern = /\((.+?)\)/g;
+let result = '';
+
+let match = pattern.exec(text);
+while (match!=null) {
+    result+=match[1]+ ';'
+    match = pattern.exec(text);
+}
+
+  return result;
+}
