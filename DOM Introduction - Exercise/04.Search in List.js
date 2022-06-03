bde3361887ecbaf08f80e722b9c19f7e275b0585
Ninkov <ninkov82@gmail.com>
2022-06-04 00:03:41 +0300

    Search in List
    An HTML page holds a list of towns, a search box, and a [Search] button. Implement the search function to bold and underline the items from the list which include the text from the search box. Also, print the number of items the current search matches in the format `${matches} matches found`.
    Note: It is necessary to clear the results of the previous search.
    Write your JavaScript code in this file:

---
+++ DOM Introduction - Exercise/04.Search in List.js
@@ -0,0 +1,21 @@
+function search() {
+  let liElements = document.querySelectorAll("ul#towns li");
+  let resultElement = document.getElementById("result");
+  let searchText = document.getElementById("searchText").value;
+  let matches = 0;
+
+  for (const element of liElements) {
+    element.style.fontWeight = "normal";
+    element.style.textDecoration = "";
+  }
+
+  for (let element of liElements) {
+    let text = element.textContent;
+    if (text.match(searchText)) {
+      matches++;
+      element.style.fontWeight = "bold";
+      element.style.textDecoration = "underline";
+    }
+  }
+  resultElement.textContent = `${matches} matches found`;
+}
