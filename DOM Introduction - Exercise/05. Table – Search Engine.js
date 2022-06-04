34fed4ecc09939d800a5d0488274a45c1725d30b
Ninkov <ninkov82@gmail.com>
2022-06-04 11:01:46 +0300

    Table – Search Engine
    Write a function that searches in a table by given input.
     
    When the "Search" button is clicked, go through all cells in the table except for the first row (Student name, Student email, and Student course) and check if the given input has a match (check for both full words and single letters).
    If any of the rows contain the submitted string, add a class select to that row. Note that more than one row may contain the given string. 
    Оtherwise, if there is no match, nothing should happen.
    Note: After every search ("Search" button is clicked), clear the input field and remove all already selected classes (if any) from the previous search, for the new search to contain only the new result.

---
+++ DOM Introduction - Exercise/05. Table – Search Engine.js
@@ -0,0 +1,16 @@
+function solve() {
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
+ 
+  let rows = document.querySelectorAll("tbody tr");
+  let input = document.getElementById("searchField");
+
+  function onClick() {
+     for (const row of rows) {
+        row.className='';
+        if (row.innerHTML.includes(input.value)) {
+           row.className='select';
+        }
+     }
+     input.value='';
+  }
+}
