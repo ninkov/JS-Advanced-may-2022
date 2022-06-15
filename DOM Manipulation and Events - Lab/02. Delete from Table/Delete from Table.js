e098828b5d6f2c1a8fb3863bc31887967ed9f1b3
Ninkov <ninkov82@gmail.com>
2022-06-15 23:03:54 +0300

    Delete from Table
    Write a program that takes an email from an input field and deletes the matching row from a table. 
    •	If entry is found, the textContent in the element with id="result" must be set to "Deleted."
    •	Otherwise, an error should be displayed in a <div> with id="result". The error should be "Not found." 
    Submit only the deleteByEmail() function in Judge. 
    Input/Output
    There will be no input/output, your program should instead modify the DOM of the given HTML document.

---
+++ DOM Manipulation and Events - Lab/02. Delete from Table/Delete from Table.js
@@ -0,0 +1,25 @@
+function deleteByEmail() {
+  //select input field and read value
+  const inputValue = document.querySelector('input[name="email"]').value;
+
+  const rows = [...document.querySelectorAll("tbody tr")];
+  //repeat for every table row
+  // -- select second column
+  // -- compare text content with input value
+  // -- if they are equal - remove current row
+  let found = false;
+  for (const row of rows) {
+    if (row.children[1].textContent == inputValue) {
+      const parent = row.parentElement;
+      parent.removeChild(row);
+      found = true;
+    }
+  }
+
+  //display result
+  if (found) {
+    document.getElementById("result").textContent = "Deleted.";
+  } else {
+    document.getElementById("result").textContent = "Not found.";
+  }
+}
