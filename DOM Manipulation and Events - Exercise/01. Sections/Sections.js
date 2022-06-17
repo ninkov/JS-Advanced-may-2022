099c4a0eac0cf874e1c869395b2fe67c9fcde62a
Ninkov <ninkov82@gmail.com>
2022-06-17 10:29:45 +0300

    sections
    You will receive an array of strings. For each string, create a div with a paragraph with the string in it. Each paragraph is initially hidden (display:none). Add a click event listener to each div that displays the hidden paragraph. Finally, you should append all divs to the element with an id "content".

---
+++ DOM Manipulation and Events - Exercise/01. Sections/Sections.js
@@ -0,0 +1,19 @@
+function create(words) {
+   const content = document.getElementById('content');
+   for (const word of words) {
+      const div = document.createElement('div');
+      const p = document.createElement('p');
+      p.textContent = word;
+      p.style.display = 'none';
+      div.appendChild(p);
+
+      div.addEventListener('click',show)
+
+      content.appendChild(div);
+
+      function show(event) {
+
+         event.currentTarget.children[0].style.display='';
+      }
+   }
+}
