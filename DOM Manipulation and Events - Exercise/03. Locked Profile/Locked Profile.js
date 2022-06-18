b1b75394f90cbd5fc49d02b542ca8b84975ed785
Ninkov <ninkov82@gmail.com>
2022-06-18 21:00:12 +0300

    Locked Profile
    In this problem, you should create a JS functionality that shows and hides the additional information about users.
     
    When one of the [Show more] buttons is clicked, the hidden information inside the div should
    be shown, only if the profile is not locked! If the current profile is locked, nothing should happen.
     
    If the hidden information is displayed and we lock the profile again, the [Hide it] button should not be working! 
    Otherwise, when the profile is unlocked and we click on the [Hide it] button, the new fields must hide again.

---
+++ DOM Manipulation and Events - Exercise/03. Locked Profile/Locked Profile.js
@@ -0,0 +1,21 @@
+function lockedProfile() {
+  [...document.querySelectorAll(".profile button")].forEach((b) =>
+    b.addEventListener("click", toggle)
+  );
+  function toggle(e) {
+    const profile = e.target.parentElement;
+    const isActive = profile.querySelector(
+      'input[type="radio"][value="unlock"]'
+    ).checked;
+    if (isActive) {
+      const div = profile.querySelector('div');
+     if (e.target.textContent=='Show more') {
+        div.style.display='block';
+        e.target.textContent="Hide it"
+     }else{
+        div.style.display='none';
+        e.target.textContent="Show more"
+     }
+    }
+  }
+}
